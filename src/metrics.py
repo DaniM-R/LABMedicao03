8a304399da43ae779ae164c79706210b1145b910
dalila <dalilamv99@outlook.com>
2023-04-12 13:31:41 -0300

    entrega 1 finalizada

--- src/metrics.py
+++ src/metrics.py
@@ -6,6 +6,14 @@ import requests
 import os
 from dotenv import load_dotenv
 import csv
+import markdown
+
+
+def get_time_diff(start_time, end_time):
+    diff_seconds = (end_time - start_time).total_seconds()
+    diff_hours, diff_minutes = divmod(diff_seconds, 3600)
+    diff_minutes //= 60
+    return "{:.0f}h{:02.0f}m".format(diff_hours, diff_minutes)
 
 load_dotenv()
 
@@ -18,6 +26,7 @@ query ($after: String) {
   repository(owner: "donnemartin", name: "system-design-primer") {
     pullRequests(first: 100, states: [MERGED,CLOSED], after: $after) {
       nodes {
+        id
         title
         url
         author {
@@ -29,6 +38,7 @@ query ($after: String) {
         closedAt
         additions
         deletions
+        state
         participants {
           totalCount
         }
@@ -38,7 +48,6 @@ query ($after: String) {
         files {
           totalCount
         }
-        state
         reviews {
           totalCount
         }
@@ -58,30 +67,56 @@ variables = {
 
 
 headers = {"Authorization": "Bearer " + token}
+row_data = []
 while True:
     response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
     
     data = json.loads(response.text)
+    if response.status_code == 200:
+        page_info = data['data']['repository']['pullRequests']['pageInfo']
 
-    page_info = data['data']['repository']['pullRequests']['pageInfo']
+        prs = data['data']['repository']['pullRequests']['nodes']
+        for pr in prs:
+            title = pr["title"]
+            created_at = datetime.strptime(
+                    pr['createdAt'], '%Y-%m-%dT%H:%M:%SZ')
+            if pr["state"] == 'CLOSED':
+                closed_at = datetime.strptime(pr['closedAt'], '%Y-%m-%dT%H:%M:%SZ')
+                review_time = get_time_diff(created_at, closed_at)
+            else:
+                merged_at = datetime.strptime(pr['mergedAt'], '%Y-%m-%dT%H:%M:%SZ')
+                review_time = get_time_diff(created_at, merged_at)
+            
+            markdown_body = markdown.markdown(pr['body'])
+            with open("src/pull_request_body.md", "w") as file:
+                file.write(markdown_body)
+            with open("src/pull_request_body.md", "r") as file:
+                content = file.read()
+                num_caracteres = len(content)
+            num_arquivos = pr["files"]["totalCount"]
+            num_additions = pr["additions"]
+            num_deletions = pr["deletions"]
+            num_participants = pr["participants"]["totalCount"]
+            num_comments = pr["comments"]["totalCount"]
+            pr_id = pr["id"]
+            state = pr["state"]
+            name_owner = "donnemartin/system-design-primer" # temporario
 
-    prs = data['data']['repository']['pullRequests']['nodes']
-    for pr in prs:
-        title = pr["title"]
-        created_at = datetime.strptime(
-                pr['createdAt'], '%Y-%m-%dT%H:%M:%SZ')
-        if pr["state"] == 'CLOSED':
-            closed_at = datetime.strptime(pr['closedAt'], '%Y-%m-%dT%H:%M:%SZ')
-            review_time = (closed_at - created_at).total_seconds() / 3600
-        else:
-            merged_at = datetime.strptime(pr['mergedAt'], '%Y-%m-%dT%H:%M:%SZ')
-            review_time = (merged_at - created_at).total_seconds() / 3600
-
-    if not page_info['hasNextPage']:
-        break
+            row = [name_owner, pr_id, title, state, review_time, num_caracteres, num_arquivos, num_additions, num_deletions, num_participants, num_comments]
+            row_data.append(row)
+        if not page_info['hasNextPage']:
+            break
+    else:
+        continue
+      
 
     variables['after'] = page_info['endCursor']
         
-
+  
+with open('metricas.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['name_owner', 'pr_id', 'title', 'state', 'review_time', 'num_caracteres', 'num_arquivos', 'num_additions', 'num_deletions', 'num_participants', 'num_comments'])
+    for row in row_data:
+        writer.writerow(row)
 
 
---
+++ src/pull_request_body.md
@@ -0,0 +1,12 @@
+<p>Linking to Wayback Machine since it seems the domain expired?</p>
+<h2>Review the Contributing Guidelines</h2>
+<p>Before submitting a pull request, verify it meets all requirements in the <a href="https://github.com/donnemartin/system-design-primer/blob/master/CONTRIBUTING.md">Contributing Guidelines</a>.</p>
+<h3>Translations</h3>
+<p>See the <a href="https://github.com/donnemartin/system-design-primer/blob/master/CONTRIBUTING.md">Contributing Guidelines</a>.  Verify you've:</p>
+<ul>
+<li>Tagged the <a href="https://github.com/donnemartin/system-design-primer/blob/master/TRANSLATIONS.md">language maintainer</a></li>
+<li>Prefixed the title with a language code<ul>
+<li>Example: "ja: Fix ..."</li>
+</ul>
+</li>
+</ul>
